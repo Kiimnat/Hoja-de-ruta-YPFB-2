ba1b59ad3025b5c56d23b912e53de70edb15130f
Kiimnat <kimponce672@gmail.com>
2025-08-06 00:42:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -37,24 +37,4 @@ document.getElementById('correspondenciaForm').addEventListener('submit', async 
   }
 
   try {
-    const logo = await cargarImagen('logo-ypfb.png');
-    doc.addImage(logo, 'PNG', 10, 10, 30, 30);
-  } catch (error) {
-    alert(error.message + ". El PDF se generará sin logo.");
-  }
-
-  doc.setFontSize(18);
-  doc.text("Yacimientos Petrolíferos Fiscales Bolivianos", 50, 20);
-  doc.setFontSize(14);
-  doc.text("Hoja Única de Correspondencia Externa", 50, 28);
-
-  doc.setFontSize(10);
-
-  // Negritas en etiquetas constantes (remitente, cargo, destinatario, cargo destinatario, referencia)
-  doc.setFont("helvetica", "bold");
-  doc.text(`De:`, 10, 58);
-  doc.text(`Cargo:`, 100, 58);
-  doc.text(`Destinatario:`, 10, 66);
-  doc.text(`Cargo destinatario:`, 100, 66);
-  doc.text(`Referencia:`, 10, 74);
-  doc.setFont("helvetica", "normal");
+    cons
